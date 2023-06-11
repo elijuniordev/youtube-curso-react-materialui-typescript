48169500f66a9f4076a9ad75fe5cd8e37190820f
Eli Junior <juusoares99@gmail.com>
2023-06-11 10:33:04 -0300

    feat: criação da página de listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,32 +1,66 @@
-import { Typography } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+
+import { Icon, IconButton, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
+
 import { FerramentasDaListagem } from '../../shared/components';
-import { useSearchParams } from 'react-router-dom';
-import { useEffect, useMemo } from 'react';
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+
 import { useDebounce } from '../../shared/hooks';
+import { Environment } from '../../shared/environment';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const { debounce } = useDebounce(3000);
+  const { debounce } = useDebounce();
+  const navigate = useNavigate();
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1');
+  }, [searchParams]);
+
   useEffect(() => {
+    setIsLoading(true);
 
     debounce(() => {
-      PessoasService.getAll(1, busca)
+      PessoasService.getAll(pagina, busca)
         .then((result) => {
+          setIsLoading(false);
           if (result instanceof Error) {
             alert(result.message);
           } else {
             console.log(result);
+
+            setTotalCount(result.totalCount);
+            setRows(result.data);
           }
         });
     });
-  }, [busca]);
+  }, [busca, pagina]);
+
+  const handleDelete = (id: number) => {
+    if (confirm('Deseja realmente excluir?'))
+      PessoasService.deleteById(id)
+        .then(result => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            setRows(oldRows => [
+              ...oldRows.filter(oldRow => oldRow.id !== id),
+            ]);
+            alert('Excluído com sucesso!');
+          }
+        });
+  };
+
 
   return (
     <LayoutBaseDePagina
@@ -36,13 +70,74 @@ export const ListagemDePessoas: React.FC = () => {
           textoBotaoNovo='Nova'
           mostrarInputBusca
           textoDaBusca={busca}
-          aoMudarTextoDeBusca={texto => setSearchParams({busca: texto}, {replace: true})}
+          aoMudarTextoDeBusca={texto => setSearchParams({busca: texto, pagina: '1' }, {replace: true})}
         />
       )}
     >
-      <Typography>
-        Coisas da Página Cidades
-      </Typography>
+      
+      <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>
+                  <IconButton size='small' onClick={() => handleDelete(row.id)}>
+                    <Icon>
+                      delete
+                    </Icon>
+                  </IconButton>
+                  <IconButton size='small' onClick={() => navigate(`/pessoas/detalhe/${row.id}`)}>
+                    <Icon>
+                      edit
+                    </Icon>
+                  </IconButton>
+                </TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+
+          </TableBody>
+
+          { totalCount === 0 && !isLoading && (
+            <caption>
+              {Environment.LISTAGEM_VAZIA}
+            </caption>
+          )}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3} >
+                  <LinearProgress variant='indeterminate'/>
+                </TableCell>
+              </TableRow>
+            )}     
+
+            {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+              <TableRow>
+                <TableCell colSpan={3} >
+                  <Pagination 
+                    page={pagina}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                    onChange={(_, newPage) => setSearchParams({busca, pagina: newPage.toString() }, {replace: true})}
+                  />
+                </TableCell>
+              </TableRow>
+            )}   
+
+          </TableFooter>
+
+        </Table>
+      </TableContainer>
         
     </LayoutBaseDePagina>
   );
